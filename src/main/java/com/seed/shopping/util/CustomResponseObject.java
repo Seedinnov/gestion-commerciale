6b9ca1d11ab92937be973014378c4d32e9607056
Ulrich <tiayo.pro@gmail.com>
2018-05-17 22:11:40 +0100

    Run codes for part 2 OK

--- src/main/java/com/seed/shopping/util/CustomResponseObject.java
+++ src/main/java/com/seed/shopping/util/CustomResponseObject.java
@@ -7,9 +7,7 @@ package com.seed.shopping.util;
 
 import com.seed.shopping.model.Article;
 import com.seed.shopping.model.Buying;
-import com.seed.shopping.model.BuyingArticle;
 import com.seed.shopping.model.Selling;
-import com.seed.shopping.model.SellingArticle;
 import java.util.List;
 
 /**
@@ -20,19 +18,15 @@ public class CustomResponseObject {
 
     private List<Article> articles;
     private List<Buying> buyings;
-    private List<BuyingArticle> buyingArticles;
     private List<Selling> sellings;
-    private List<SellingArticle> sellingArticles;
 
     public CustomResponseObject() {
     }
 
-    public CustomResponseObject(List<Article> articles, List<Buying> buyings, List<BuyingArticle> buyingArticles, List<Selling> sellings, List<SellingArticle> sellingArticles) {
+    public CustomResponseObject(List<Article> articles, List<Buying> buyings, List<Selling> sellings) {
         this.articles = articles;
         this.buyings = buyings;
-        this.buyingArticles = buyingArticles;
         this.sellings = sellings;
-        this.sellingArticles = sellingArticles;
     }
 
     public List<Article> getArticles() {
@@ -51,28 +45,12 @@ public class CustomResponseObject {
         this.buyings = buyings;
     }
 
-    public List<BuyingArticle> getBuyingArticles() {
-        return buyingArticles;
-    }
-
-    public void setBuyingArticles(List<BuyingArticle> buyingArticles) {
-        this.buyingArticles = buyingArticles;
-    }
-
     public List<Selling> getSellings() {
         return sellings;
     }
 
     public void setSellings(List<Selling> sellings) {
         this.sellings = sellings;
-    }
-
-    public List<SellingArticle> getSellingArticles() {
-        return sellingArticles;
-    }
-
-    public void setSellingArticles(List<SellingArticle> sellingArticles) {
-        this.sellingArticles = sellingArticles;
     }
 
 }
